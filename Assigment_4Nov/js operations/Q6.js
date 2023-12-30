5d3b1823e71291e92e373d4c390a2c9cc4614b51
Annesh999 <145187528+Annesh999@users.noreply.github.com>
2023-12-30 13:00:02 +0530

    Add files via upload

---
+++ Assigment_4Nov/js operations/Q6.js
@@ -0,0 +1,6 @@
+// Q6. Write a Javascript program to calculate the Body Mass Index (BMI) using the formula BMI = weight (kg)/
+// height * height.
+let weight=90;
+let height= 150;
+let result = weight / height*height;
+console.log("BMI =",result)
---
+++ Assigment_4Nov/js operations/Q7.js
@@ -0,0 +1,6 @@
+// Q7. Write a program in JavaScript to calculate the area of a circle given its radius value of 10. Use appropriate
+// arithmetic operators.
+
+let redius=10;
+let Area= Math.PI*Math.pow(redius,2);
+console.log(Area);
